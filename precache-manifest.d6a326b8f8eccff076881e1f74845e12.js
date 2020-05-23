8fddf41284d96c378dae2f37107d5ba484e83316
cococov <juank.yea@hotmail.com>
2020-05-23 03:00:15 -0400

    Updates

---
+++ precache-manifest.d6a326b8f8eccff076881e1f74845e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c8cddc2302b00a2fddb5dfd27fb47c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6013d92d907cc225a83",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bc6dba84458a9ada26a",
+    "url": "/static/js/2.27e6675c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27e6675c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6013d92d907cc225a83",
+    "url": "/static/js/main.e44eb487.chunk.js"
+  },
+  {
+    "revision": "9e09c104ebdbfa0ac0c9",
+    "url": "/static/js/runtime-main.59d4219f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
